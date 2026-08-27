9408e26a8abf57bed9fae7baec1028818d7878c1
vanapallimanojkumar2007-alt <vanapallimanojkumar2007@gmail.com>
2026-08-27 15:15:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const sum = (a, b) => a + b;
+console.log("output of sum is")
+console.log(sum(2, 3)); // 5
+document.writeln("sum is" + sum(2,3) +"<br>");
+const sub = (c, d) => c - d;
+console.log("output of sub is")
+console.log(sub(2, 3)); // 5
+document.writeln("sub is" + sub(2,3) +"<br>");
+const mul = (e, f) => e * f;
+console.log("output of mul is")
+console.log(mul(2, 3)); // 6
+document.writeln("mul is" + mul(2,3)+"<br>");
+const div = (g, h) => g / h;
+console.log("output of div is")
+console.log(div(2, 3)); // 5
+document.writeln("div is" + div(2,3)+"<br>");
+const mod = (i, j) => i % j;
+console.log("output of mod is")
+console.log(mod(2, 3));
+document.writeln("mod is" + mod(2,3)+"<br>");
+const pow = (k, l) => k ** l;
+console.log("output of pow is")
+console.log(pow(2, 3));
+document.writeln("pow is" + pow(2,3)+"<br>");
